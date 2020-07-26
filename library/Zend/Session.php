f2deff56b8b63c8fcbfed46b695aedb75dc59e4a
Emelianov Sergei <sergey@emelyanov.su>
2020-07-26 20:38:27 +1000

    Fixed getting number of bits in encoded session ID since PHP 7.1 (parameter session.hash_bits_per_character was deleted in PHP 7.1.0)

--- library/Zend/Session.php
+++ library/Zend/Session.php
@@ -537,7 +537,10 @@ class Zend_Session extends Zend_Session_Abstract
             }
         }
 
-        $hashBitsPerChar = ini_get('session.hash_bits_per_character');
+        $hashBitsPerChar = ini_get('session.sid_bits_per_character');
+        if (!$hashBitsPerChar) {
+          $hashBitsPerChar = ini_get('session.hash_bits_per_character');
+        }
         if (!$hashBitsPerChar) {
             $hashBitsPerChar = 5; // the default value
         }
