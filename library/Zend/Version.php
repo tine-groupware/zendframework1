b8d972a1f67fa84152242ba83dd5bf5f9f23e011
George Appleton <me@georgeappleton.co.uk>
2021-02-07 23:53:55 +0000

    Version bump

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.18.11';
+    const VERSION = '1.19.0';
 
     /**
      * The latest stable version Zend Framework available
