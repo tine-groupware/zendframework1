a2f3dcb4da67820ddfd888a1fc2fc2a045c61111
Philipp Schüle <p.schuele@metaways.de>
2021-03-29 10:08:25 +0200

    fix(Mail/Imap): skip empty tokens
    ... to prevent "Uninitialized string offset"

--- library/Zend/Mail/Protocol/Imap.php
+++ library/Zend/Mail/Protocol/Imap.php
@@ -241,64 +241,66 @@ class Zend_Mail_Protocol_Imap
         $line = rtrim($line) . ' ';
         while (($pos = strpos($line, ' ')) !== false) {
             $token = substr($line, 0, $pos);
-            if(($bracePos = strpos($token, ')(')) !== false) {
+            if (($bracePos = strpos($token, ')(')) !== false) {
                 $token = substr($token, 0, $bracePos +1);
                 $pos = $bracePos;
             }
-            while ($token[0] == '(') {
-                array_push($stack, $tokens);
-                $tokens = array();
-                $token = substr($token, 1);
-                $line  = substr($line, 1);
-                $pos--;
-            }
-            if ($token[0] == '"') {
-                if (preg_match('%^\(*"((.|\\\\|\\")*?)" *%', $line, $matches)) {
-                    $tokens[] = $matches[1];
-                    $line = substr($line, strlen($matches[0]));
-                    continue;
+            if (strlen($token) > 0) {
+                while ($token[0] == '(') {
+                    array_push($stack, $tokens);
+                    $tokens = array();
+                    $token = substr($token, 1);
+                    $line = substr($line, 1);
+                    $pos--;
                 }
-            }
-            if ($token[0] == '{') {
-                $endPos = strpos($token, '}');
-                $chars = substr($token, 1, $endPos - 1);
-                if (is_numeric($chars)) {
-                    $token = '';
-                    while (strlen($token) < $chars) {
-                        $token .= $this->_nextLine();
+                if ($token[0] == '"') {
+                    if (preg_match('%^\(*"((.|\\\\|\\")*?)" *%', $line, $matches)) {
+                        $tokens[] = $matches[1];
+                        $line = substr($line, strlen($matches[0]));
+                        continue;
                     }
-                    $line = '';
-                    if (strlen($token) > $chars) {
-                        $line = substr($token, $chars);
-                        $token = substr($token, 0, $chars);
-                    } else {
-                        $line .= $this->_nextLine();
+                }
+                if ($token[0] == '{') {
+                    $endPos = strpos($token, '}');
+                    $chars = substr($token, 1, $endPos - 1);
+                    if (is_numeric($chars)) {
+                        $token = '';
+                        while (strlen($token) < $chars) {
+                            $token .= $this->_nextLine();
+                        }
+                        $line = '';
+                        if (strlen($token) > $chars) {
+                            $line = substr($token, $chars);
+                            $token = substr($token, 0, $chars);
+                        } else {
+                            $line .= $this->_nextLine();
+                        }
+                        $tokens[] = $token;
+                        $line = trim($line) . ' ';
+                        continue;
                     }
-                    $tokens[] = $token;
-                    $line = trim($line) . ' ';
-                    continue;
                 }
-            }
-            if ($stack && $token[strlen($token) - 1] == ')') {
-                // closing braces are not seperated by spaces, so we need to count them
-                $braces = strlen($token);
-                $token = rtrim($token, ')');
-                // only count braces if more than one
-                $braces -= strlen($token) + 1;
-                // only add if token had more than just closing braces
-                if (rtrim($token) != '') {
-                    $tokens[] = rtrim($token);
-                }
-                $token = $tokens;
-                $tokens = array_pop($stack);
-                // special handline if more than one closing brace
-                while ($braces-- > 0) {
-                    $tokens[] = $token;
+                if ($stack && $token[strlen($token) - 1] == ')') {
+                    // closing braces are not seperated by spaces, so we need to count them
+                    $braces = strlen($token);
+                    $token = rtrim($token, ')');
+                    // only count braces if more than one
+                    $braces -= strlen($token) + 1;
+                    // only add if token had more than just closing braces
+                    if (rtrim($token) != '') {
+                        $tokens[] = rtrim($token);
+                    }
                     $token = $tokens;
                     $tokens = array_pop($stack);
+                    // special handline if more than one closing brace
+                    while ($braces-- > 0) {
+                        $tokens[] = $token;
+                        $token = $tokens;
+                        $tokens = array_pop($stack);
+                    }
                 }
+                $tokens[] = $token;
             }
-            $tokens[] = $token;
             $line = substr($line, $pos + 1);
         }
 
