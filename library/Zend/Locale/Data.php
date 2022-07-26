498735b5b304f6a1ee04d2e02ed548ef43d7abe3
Jimbolino <Jimbolino@users.noreply.github.com>
2022-07-26 11:10:43 +0200

    fix: Deprecated: urlencode(): Passing null to parameter #1 ($string) of type string is deprecated in /Zend/Locale/Data.php

--- library/Zend/Locale/Data.php
+++ library/Zend/Locale/Data.php
@@ -982,7 +982,7 @@ class Zend_Locale_Data
         if (is_array($value)) {
             $val = implode('_' , $value);
         }
-        $val = urlencode($val);
+        $val = urlencode((string) $val);
         $id  = self::_filterCacheId('Zend_LocaleC_' . $locale . '_' . $path . '_' . $val);
         if (!self::$_cacheDisabled && ($result = self::$_cache->load($id))) {
             return unserialize($result);
