dd450081d35faf0b912d9a100a6919c164d74fd3
hungtd <trinhduchung@gmail.com>
2022-11-23 11:15:45 +0700

    Add missing properties to classes tests/Zend/DateTest.php
    PHP 8.2: Dynamic Properties are deprecated fix
    
    Reference: https://php.watch/versions/8.2/dynamic-properties-deprecated
    
    Verified by:
    docker run -u "$(id -u):$(id -g)" -v $(pwd):$(pwd) -w $(pwd) -t --rm php:8.2-rc-cli-alpine -d memory_limit=-1 ./bin/phpunit -v --bootstrap tests/TestHelper.php tests/Zend/DateTest.php

--- tests/Zend/DateTest.php
+++ tests/Zend/DateTest.php
@@ -56,9 +56,13 @@ require_once 'Zend/TimeSync.php';
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @group      Zend_Date
  */
-#[AllowDynamicProperties]
 class Zend_DateTest extends TestCase
 {
+    /**
+     * @var string|mixed
+     */
+    protected $originalTimezone;
+
     private $_cache = null;
     private $_orig = [];
 
