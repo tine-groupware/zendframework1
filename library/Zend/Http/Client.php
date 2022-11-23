f65433fe68db440e29c07b09d56bc81c12b52dff
hungtd <trinhduchung@gmail.com>
2022-11-24 00:38:48 +0700

    Add missing properties to classes Zend/Http/*
    PHP 8.2: Dynamic Properties are deprecated fix
    
    Reference: https://php.watch/versions/8.2/dynamic-properties-deprecated
    
    Verified by:
    docker run -u "$(id -u):$(id -g)" -v $(pwd):$(pwd) -w $(pwd) -t --rm php:8.2-rc-cli-alpine -d memory_limit=-1 ./bin/phpunit -v --bootstrap tests/TestHelper.php tests/Zend/Http/AllTests.php

--- library/Zend/Http/Client.php
+++ library/Zend/Http/Client.php
@@ -69,9 +69,13 @@ require_once 'Zend/Http/Response/Stream.php';
  * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-#[AllowDynamicProperties]
 class Zend_Http_Client
 {
+    /**
+     * @var mixed|string|bool
+     */
+    protected $_stream_name;
+
     /**
      * HTTP request methods
      */
