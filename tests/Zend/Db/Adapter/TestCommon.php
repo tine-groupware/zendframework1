436a760ec92816692655fd0d6c058caad068a3de
hungtd <trinhduchung@gmail.com>
2022-12-08 17:25:46 +0700

    Fix bug: 'This test did not perform any assertions'
    Issue raise here:
    - https://github.com/hungtrinh/zf1-future/actions/runs/3646749004/jobs/6158176190#step:7:973
    - https://github.com/hungtrinh/zf1-future/actions/runs/3646749004/jobs/6158176190#step:7:981

--- tests/Zend/Db/Adapter/TestCommon.php
+++ tests/Zend/Db/Adapter/TestCommon.php
@@ -48,6 +48,7 @@ abstract class Zend_Db_Adapter_TestCommon extends Zend_Db_TestSetup
     /**
      * Test AUTO_QUOTE_IDENTIFIERS option
      * Case: Zend_Db::AUTO_QUOTE_IDENTIFIERS = true
+     * @doesNotPerformAssertions
      */
     public function testAdapterZendConfig()
     {
@@ -61,6 +62,7 @@ abstract class Zend_Db_Adapter_TestCommon extends Zend_Db_TestSetup
     /**
      * Test empty adapterNamespace issue
      * Case: Zend_Db::AUTO_QUOTE_IDENTIFIERS = true
+     * @doesNotPerformAssertions
      */
     public function testAdapterZendConfigEmptyNamespace()
     {
